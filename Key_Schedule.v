725032f9ce7e864e1bea6e887effa5a826446121
tellosss <sandratellosalinas@outlook.es>
2021-06-03 18:10:31 -0500

    Version 4, no funciona la simulacion

--- Key_Schedule.v
+++ Key_Schedule.v
@@ -30,13 +30,13 @@ module Key_Schedule(
 	 
 	 // Embedded signals
 	 reg [7:0] 		expanded_key		[0:175];
-	 reg [7:0] 		expanded_key_aux	[0:175];
-	 reg [7:0] 		Key_temp 			[0:15];
-	 reg [7:0] 		temp_aux 			[0:3];	 
-	 reg [7:0] 		temp 				[0:3];
-//	 reg [7:0] 		temp 				[0:3];
-	 reg [7:0] 		aux;
-	 integer i = 1;
+	 reg [7:0] 		Key_temp     		[0:15];
+		
+	 integer j;
+	 integer c;
+	 integer z;
+	 genvar k;
+	 
 	 
 	 //Constants
 	 // Original key
@@ -47,7 +47,7 @@ module Key_Schedule(
 							8'h09, 8'hcf, 8'h4f, 8'h3c};
 	 
 	 //S-box
-	 parameter [7:0] Sbox 			[0:255] = {	
+	 parameter [7:0] SBox 			[0:255] = {	
 							8'h63,8'h7c,8'h77,8'h7b,8'hf2,8'h6b,8'h6f,8'hc5,8'h30,8'h01,8'h67,8'h2b,8'hfe,8'hd7,8'hab,8'h76,
 							8'hca,8'h82,8'hc9,8'h7d,8'hfa,8'h59,8'h47,8'hf0,8'had,8'hd4,8'ha2,8'haf,8'h9c,8'ha4,8'h72,8'hc0,
 							8'hb7,8'hfd,8'h93,8'h26,8'h36,8'h3f,8'hf7,8'hcc,8'h34,8'ha5,8'he5,8'hf1,8'h71,8'hd8,8'h31,8'h15,
@@ -70,135 +70,191 @@ module Key_Schedule(
 							8'h20, 8'h40, 8'h80, 8'h1B, 8'h36};
 							
 	
-​
-	// Initialize the first 16 blocks with the original key
-	integer j;
-	integer a;
-	integer x;
-	integer y;
-	integer z;
-	
-	initial
-	begin
-		for (j = 0; j < 16; j = j + 1)
-			expanded_key[j] = original_key[j];
-	end	
-	
-	
-	genvar c;
-	generate
-	for (c = 16; c < 176; c = c + 1)
-	begin: loop
-		always @(*)
-		begin
-			// Copy the last 4-byte block to the temporary array
-			for (a = 0; a < 4; a = a + 1)
-			begin
-				temp[a] = expanded_key[a+c-4]; 
-			end
-			
-			if (c%16 == 0)
-			begin
-				aux = temp[0];
-				temp[0] = temp[1];
-				temp[1] = temp[2];
-				temp[2] = temp[3];
-				temp[3] = aux;	
-​
-				for (x = 0; x < 4; x = x + 1)
-				begin
-					temp_aux[x] = temp[x];
-					temp[x] = Sbox[temp_aux[x]]; 
-				end
-				
-				//Adding (XOR) this result to the word 4 positions earlier plus a round constant Rcon
-				temp[0] = temp[0] ^ Rcon[i];
-				i = i + 1;
-			end
-			
-			//Adding (XOR) to the temp vector
-			for (y = 0; y < 4; y = y + 1)
-			begin
-				expanded_key_aux[c] = expanded_key[c-16];
-				expanded_key[c] = expanded_key_aux[c] ^ temp[y];
-			end
-			
-		end
-	end
-	endgenerate
-		
-​
-	always @(posedge(Clk) or posedge(Rst))
+
+//	// Initialize the first 16 blocks with the original key
+//	integer j;
+//	integer a;
+//	integer x;
+//	integer y;
+//	integer z;
+//	integer k;
+//	
+//	initial
+//	begin
+//		for (j = 0; j < 16; j = j + 1)
+//			expanded_key[j] = original_key[j];
+//			
+//	   k = 0;
+//	end	
+//	
+//	
+//	genvar c;
+//	generate
+//	for (c = 16; c < 176; c = c+4)
+//	begin: loop
+//		always @(*)
+//		begin
+//			// Copy the last 4-byte block to the temporary array
+//			for (a = 0; a < 4; a = a + 1)
+//			begin
+//				temp[a] = expanded_key[a+c-4]; 
+//			end
+//			
+//			if (c%16 == 0)
+//			begin
+//				aux[k] = temp[k*4];
+//				temp[k*4] = temp[k*4+1];
+//				temp[k*4+1] = temp[k*4+2];
+//				temp[k*4+2] = temp[k*4+3];
+//				temp[k*4+3] = aux[k];	
+//
+//				for (x = 0; x < 4; x = x + 1)
+//				begin
+//					temp_aux[k*4+x] = temp[k*4+x];
+//					temp[k*4+x] = Sbox[temp_aux[k*4+x]]; 
+//				end
+//				
+////				//Adding (XOR) this result to the word 4 positions earlier plus a round constant Rcon
+//				temp_aux_rcon[k*4] = temp[k*4];
+//				temp[k*4] = temp_aux_rcon[k*4] ^ Rcon[i];
+//				i = i + 1;
+//				k = k + 1;
+//			end
+//			
+////			//Adding (XOR) to the temp vector
+////			for (y = 0; y < 4; y = y + 1)
+////			begin
+////				expanded_key_aux[c+y] = expanded_key[c+y-16];
+////				expanded_key[c+y] = expanded_key_aux[c+y] ^ temp[y];
+//				
+////			end
+//			
+//		end
+//	end
+//	endgenerate
+
+
+
+	always @(posedge(Clk))
 	begin
 		if(Rst)
 		begin
+			for (j = 0; j < 16; j = j + 1)
+				expanded_key[j] = original_key[j];
 			Ry = 0;
-//			En = 0;
-			//Key = 128'h0;
+
 			//Intialize the expanded key with 0's
-			for (j = 0; j < 176; j = j + 1)
+			for (j = 16; j < 176; j = j + 1)
 				expanded_key[j] = 8'h00; 
 		end
-		else
+		else if(En)
 		begin
-			case(SelKey)
-				4'b0000: begin 
-								for (z = 0; z < 16; z = z + 1)
-									Key_temp[z] = expanded_key[z];
-							end
-				4'b0001: begin 
-								for (z = 16; z < 32; z = z + 1)
-									Key_temp[z-16] = expanded_key[z];
-							end
-				4'b0010: begin 
-								for (z = 32; z < 48; z = z + 1)
-									Key_temp[z-32] = expanded_key[z];
-							end
-				4'b0011: begin 
-								for (z = 48; z < 64; z = z + 1)
-									Key_temp[z-48] = expanded_key[z];
-							end
-				4'b0100: begin 
-								for (z = 64; z < 80; z = z + 1)
-									Key_temp[z-64] = expanded_key[z];
-							end
-				4'b0101: begin 
-								for (z = 80; z < 96; z = z + 1)
-									Key_temp[z-80] = expanded_key[z];
-							end
-				4'b0110: begin 
-								for (z = 96; z < 112; z = z + 1)
-									Key_temp[z-96] = expanded_key[z];
-							end
-				4'b0111: begin
-								for (z = 112; z < 128; z = z + 1)
-									Key_temp[z-112] = expanded_key[z];
-							end
-				4'b1000: begin 
-								for (z = 128; z < 144; z = z + 1)
-									Key_temp[z-128] = expanded_key[z];
-							end
-				4'b1001: begin 
-								for (z = 144; z < 160; z = z + 1)
-									Key_temp[z-144] = expanded_key[z];
-							end
-				4'b1010: begin 
-								for (z = 160; z < 176; z = z + 1)
-									Key_temp[z-160] = expanded_key[z];
-							end
-				default: begin 
-								for (z = 0; z < 16; z = z + 1)
-									Key_temp[z] = expanded_key[z];
-							end
-			endcase
-		
-			
+			for (c = 16; c < 176; c = c + 16)
+				begin
+					expanded_key[c] = SBox[expanded_key[c-3]] ^ Rcon[c/16 - 1] ^ expanded_key[c-16];
+					expanded_key[c + 1] = SBox[expanded_key[c-2]] ^ expanded_key[c-15];
+					expanded_key[c + 2] = SBox[expanded_key[c-1]] ^ expanded_key[c-14];
+					expanded_key[c + 3] = SBox[expanded_key[c-4]] ^ expanded_key[c-13];
+					
+					expanded_key[c + 4] = expanded_key[c] ^ expanded_key[c - 12];
+					expanded_key[c + 5] = expanded_key[c + 1] ^ expanded_key[c - 11];
+					expanded_key[c + 6] = expanded_key[c + 2] ^ expanded_key[c - 10];
+					expanded_key[c + 7] = expanded_key[c + 3] ^ expanded_key[c - 9];
+					
+					expanded_key[c + 8] = expanded_key[c + 4] ^ expanded_key[c - 8];
+					expanded_key[c + 9] = expanded_key[c + 5] ^ expanded_key[c - 7];
+					expanded_key[c + 10] = expanded_key[c + 6] ^ expanded_key[c - 6];
+					expanded_key[c + 11] = expanded_key[c + 7] ^ expanded_key[c - 5];
+					
+					expanded_key[c + 12] = expanded_key[c + 8] ^ expanded_key[c - 4];
+					expanded_key[c + 13] = expanded_key[c + 9] ^ expanded_key[c - 3];
+					expanded_key[c + 14] = expanded_key[c + 10] ^ expanded_key[c - 2];
+					expanded_key[c + 15] = expanded_key[c + 11] ^ expanded_key[c - 1];
+					
+					Ry = 1;
+				end
 		end
 		
-	end
+		
+		
+		case(SelKey)
+			4'b0000: begin 
+							for (z = 0; z < 16; z = z + 1)
+								Key_temp[z] = expanded_key[z];
+						end
+			4'b0001: begin 
+							for (z = 16; z < 32; z = z + 1)
+								Key_temp[z-16] = expanded_key[z];
+						end
+			4'b0010: begin 
+							for (z = 32; z < 48; z = z + 1)
+								Key_temp[z-32] = expanded_key[z];
+						end
+			4'b0011: begin 
+							for (z = 48; z < 64; z = z + 1)
+								Key_temp[z-48] = expanded_key[z];
+						end
+			4'b0100: begin 
+							for (z = 64; z < 80; z = z + 1)
+								Key_temp[z-64] = expanded_key[z];
+						end
+			4'b0101: begin 
+							for (z = 80; z < 96; z = z + 1)
+								Key_temp[z-80] = expanded_key[z];
+						end
+			4'b0110: begin 
+							for (z = 96; z < 112; z = z + 1)
+								Key_temp[z-96] = expanded_key[z];
+						end
+			4'b0111: begin
+							for (z = 112; z < 128; z = z + 1)
+								Key_temp[z-112] = expanded_key[z];
+						end
+			4'b1000: begin 
+							for (z = 128; z < 144; z = z + 1)
+								Key_temp[z-128] = expanded_key[z];
+						end
+			4'b1001: begin 
+							for (z = 144; z < 160; z = z + 1)
+								Key_temp[z-144] = expanded_key[z];
+						end
+			4'b1010: begin 
+							for (z = 160; z < 176; z = z + 1)
+								Key_temp[z-160] = expanded_key[z];
+						end
+			default: begin 
+							for (z = 0; z < 16; z = z + 1)
+								Key_temp[z] = expanded_key[z];
+						end
+		endcase
+	
+	
+	end //Always ends here
 	
 	//Output
-	assign Key[127:111] = Key_temp[0];
-​
+//	assign Key[127:120] = Key_temp[0];
+//	assign Key[119:112] = Key_temp[1];
+//	assign Key[111:104] = Key_temp[2];
+//	assign Key[103:96]  = Key_temp[3];
+//	assign Key[95:88] =   Key_temp[4];
+//	assign Key[87:80] =   Key_temp[5];
+//	assign Key[79:72] =   Key_temp[6];
+//	assign Key[71:64] =   Key_temp[7];
+//	assign Key[63:56] =   Key_temp[8];
+//	assign Key[55:48] =   Key_temp[9];
+//	assign Key[47:40] =   Key_temp[10];
+//	assign Key[39:32] =   Key_temp[11];
+//	assign Key[31:24] =   Key_temp[12];
+//	assign Key[23:16] =   Key_temp[13];
+//	assign Key[15:8]    = Key_temp[14];
+//	assign Key[7:0]     = Key_temp[15];
+	
+	//Alternatively:
+	for (k = 0; k < 15; k = k + 1)
+	begin
+		assign Key[127-(k*8):120-(k*8)] = Key_temp[k];
+	end
+	assign Key[7:0]     = Key_temp[15];
+	
 endmodule
-​
+
